157fb8ce71b284d8d671c5662959883dd40d9dac
Yusuf Filiz <y.filiz.ch@gmail.com>
2023-05-07 15:54:53 +0200

    added a game class where the cards are shuffled with a function

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Game } from 'src/models/game';
 
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
   styleUrls: ['./game.component.scss']
 })
-export class GameComponent {
+export class GameComponent implements OnInit{
   pickCardAnimation = false;
-  
+  game: Game;
+
+  ngOnInit(): void {
+    this.newGame();
+  }
+
+  constructor() {
+    this.game = new Game();
+  }
+
+  newGame(){
+    this.game = new Game();
+    console.log(this.game);
+  }
+
   takeCard(){
     this.pickCardAnimation = true;
   }
