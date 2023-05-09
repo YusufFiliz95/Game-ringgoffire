99dca9684e94b9d6f40a8c029f1b78197d5069ab
Yusuf Filiz <y.filiz.ch@gmail.com>
2023-05-09 14:08:53 +0200

    added 3 players

--- src/app/player/player.component.ts
+++ src/app/player/player.component.ts
@@ -3,10 +3,8 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-player',
   templateUrl: './player.component.html',
-  styleUrls: ['./player.component.scss']
+  styleUrls: ['./player.component.scss'],
 })
 export class PlayerComponent {
-
   @Input() name;
-
 }
