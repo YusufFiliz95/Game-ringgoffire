2825dcf98634b549d4f04224c42da40798b6dac0
Yusuf Filiz <y.filiz.ch@gmail.com>
2023-05-10 14:05:02 +0200

    you can now add players

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Game } from 'src/models/game';
+import { MatDialog } from '@angular/material/dialog';
+import { DialogAddPlayerComponent } from '../dialog-add-player/dialog-add-player.component';
 
 @Component({
   selector: 'app-game',
@@ -10,13 +12,14 @@ export class GameComponent implements OnInit {
   pickCardAnimation = false;
   currentCard: string = '';
 
+
   game: Game;
 
   ngOnInit(): void {
     this.newGame();
   }
 
-  constructor() {}
+  constructor(public dialog: MatDialog) {}
 
   newGame() {
     this.game = new Game();
@@ -35,5 +38,13 @@ export class GameComponent implements OnInit {
         this.pickCardAnimation = false;
       }, 1000);
     }
+  }
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(DialogAddPlayerComponent);
+
+    dialogRef.afterClosed().subscribe((name: string) => {
+      this.game.players.push(name);
+    });
   }
 }
