d22661db79972568510f3db0a23221e48340889c
Yusuf Filiz <y.filiz.ch@gmail.com>
2023-05-22 13:03:49 +0200

    installed angularfire

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  firebase: {
+    projectId: 'y-f-ringoffire',
+    appId: '1:670661671283:web:a1a087e1dc175b9b0fad06',
+    storageBucket: 'y-f-ringoffire.appspot.com',
+    apiKey: 'AIzaSyBwkm6v85FG4ON_WjIw1EIjJtN-hJwf0N0',
+    authDomain: 'y-f-ringoffire.firebaseapp.com',
+    messagingSenderId: '670661671283',
+  },};
